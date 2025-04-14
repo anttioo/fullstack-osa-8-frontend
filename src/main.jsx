a3a03dc4a3eae54cc6a78ecc8e3b3b7c65a0d7d9
Antti Ollikkala <antti@neksim.com>
2025-04-14 16:28:26 +0300

    added react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { ApolloProvider } from '@apollo/client'
+import { BrowserRouter } from "react-router";
+
 import App from "./App.jsx";
 
+import {client} from "./apolloClient.js";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <ApolloProvider client={client}>
+                <App/>
+            </ApolloProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
