f1f3840a4018340715f56aa9b0031121b0ce7ec1
Antti Ollikkala <antti@neksim.com>
2025-04-15 01:18:11 +0300

    8.19

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,18 +1,30 @@
 import { useQuery } from '@apollo/client'
 import {BOOKS_QUERY} from "../queries.js";
+import {useMemo, useState} from "react";
 
 export default function Books() {
+    const [selectedGenres, setSelectedGenres] = useState([])
+    const { loading, data: { allBooks } = { allBooks: [] } } = useQuery(BOOKS_QUERY)
 
-    const { loading, data } = useQuery(BOOKS_QUERY)
+    const allGenres = useMemo(() => {
+        return [...new Set(allBooks.map(b => b.genres).flat())]
+    }, [allBooks])
 
-    const books = data ? data.allBooks : []
+    const filteredBooks = selectedGenres.length === 0
+        ? allBooks
+        : allBooks.filter(b => b.genres.some(g => selectedGenres.includes(g)))
+
+    function handleGenreChange(genre, value) {
+        setSelectedGenres(!value
+            ? selectedGenres.filter(g => g !== genre)
+            : selectedGenres.concat(genre))
+    }
 
     if (loading) return <p>Loading...</p>
 
     return (
         <div>
             <h2>books</h2>
-
             <table>
                 <tbody>
                 <tr>
@@ -20,7 +32,7 @@ export default function Books() {
                     <th>author</th>
                     <th>published</th>
                 </tr>
-                {books.map((a) => (
+                {filteredBooks.map((a) => (
                     <tr key={a.title}>
                         <td>{a.title}</td>
                         <td>{a.author.name}</td>
@@ -29,6 +41,15 @@ export default function Books() {
                 ))}
                 </tbody>
             </table>
+            <h2>genres</h2>
+            <ul>
+                {allGenres.map((g) => {
+                    const selected = selectedGenres.includes(g)
+                    return <li key={g}>{g}
+                        <input type="checkbox" checked={selected} onChange={() => handleGenreChange(g, !selected)}/>
+                    </li>
+                })}
+            </ul>
         </div>
     )
 }
